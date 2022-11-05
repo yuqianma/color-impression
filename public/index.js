39f76b954618dd8cfecc6df2908c9352347a2369
Yuqian Ma <mayq0422@gmail.com>
2022-11-06 01:58:31 +0800

    feat: use camera as eyedropper to pick color

--- public/index.js
+++ public/index.js
@@ -1,131 +1,91 @@
-// revised from https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API/Taking_still_photos
-// The width and height of the captured photo. We will set the
-// width to the value defined here, but the height will be
-// calculated based on the aspect ratio of the input stream.
+const takeColorButton = document.getElementById('take-color');
+const focusedColorEl = document.getElementById("focused-color");
 
-const width = 320; // We will scale the photo width to this
-let height = 0; // This will be computed based on the input stream
+takeColorButton.addEventListener('click', () => {
+	switchView('camera-eyedropper');
+	liveCapture();
+});
+focusedColorEl.addEventListener('click', () => {
+	switchView('color-clock');
+	const colorClockView = document.getElementById('color-clock');
+	colorClockView.style.backgroundColor = focusedColorEl.style.backgroundColor;
+});
 
-// |streaming| indicates whether or not we're currently streaming
-// video from the camera. Obviously, we start at false.
-
-let streaming = false;
-
-// The various HTML elements we need to configure or control. These
-// will be set by the startup() function.
-
-let video = null;
-let canvas = null;
-let photo = null;
-let startbutton = null;
-
-function showViewLiveResultButton() {
-	if (window.self !== window.top) {
-		// Ensure that if our document is in a frame, we get the user
-		// to first open it in its own tab or window. Otherwise, it
-		// won't be able to request permission for camera access.
-		document.querySelector(".contentarea").remove();
-		const button = document.createElement("button");
-		button.textContent = "View live result of the example code above";
-		document.body.append(button);
-		button.addEventListener("click", () => window.open(location.href));
-		return true;
+function switchView(name) {
+	const views = document.getElementsByClassName("view");
+	for (let i = 0; i < views.length; i++) {
+		views[i].classList.remove("active");
 	}
-	return false;
+	document.getElementById(name).classList.add("active");
 }
 
-function startup() {
-	if (showViewLiveResultButton()) {
-		return;
-	}
-	video = document.getElementById("video");
-	canvas = document.getElementById("canvas");
-	photo = document.getElementById("photo");
-	startbutton = document.getElementById("startbutton");
 
-	navigator.mediaDevices
-		.getUserMedia({ video: true, audio: false })
-		.then((stream) => {
-			video.srcObject = stream;
-			video.play();
-		})
-		.catch((err) => {
-			console.error(`An error occurred: ${err}`);
+const CAPTURE_SIZE = { WIDTH: 720, HEIGHT: 720 };
+
+async function liveCapture() {
+	const stream = await navigator.mediaDevices.getUserMedia({
+		video: {
+			facingMode: "environment",
+			width: CAPTURE_SIZE.WIDTH,
+			height: CAPTURE_SIZE.HEIGHT
+		},
+		audio: false
+	});
+	const cameraVideo = document.getElementById("camera-video");
+	
+	const colorReader = new ColorReader(cameraVideo, focusedColorEl);
+	cameraVideo.srcObject = stream;
+	cameraVideo.play();
+}
+
+class ColorReader {
+	_started = false;
+	_dx = 0;
+	_dy = 0;
+	constructor(video, elementToPreview) {
+		this.video = video;
+		this.elementToPreview = elementToPreview;
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = 1;
+		this.canvas.height = 1;
+		this.context = this.canvas.getContext('2d');
+
+		this.video.addEventListener('playing', () => {
+			// Offset video on canvas to get the center pixel
+			this._dx = -this.video.videoWidth / 2 | 0;
+			this._dy = -this.video.videoHeight / 2 | 0;
+			this._start();
 		});
+		this.video.addEventListener('pause', () => {
+			this._stop();
+		});
+	}
 
-	video.addEventListener(
-		"canplay",
-		(ev) => {
-			if (!streaming) {
-				height = video.videoHeight / (video.videoWidth / width);
+	_readVideoCenterPixel() {
+		const { video, _dx, _dy } = this;
+		this.context.drawImage(video, _dx, _dy);
+		const imageData = this.context.getImageData(0, 0, 1, 1);
+		const [r, g, b] = imageData.data;
+		return [r, g, b];
+	}
 
-				// Firefox currently has a bug where the height can't be read from
-				// the video, so we will make assumptions if this happens.
+	_previewPixel() {
+		const pixel = this._readVideoCenterPixel();
+		const color = `#${pixel.map(c => c.toString(16).padStart(2, '0')).join('')}`;
+		this.elementToPreview.style.backgroundColor = color;
+	}
 
-				if (isNaN(height)) {
-					height = width / (4 / 3);
-				}
+	_start() {
+		this._started = true;
+		const frame = () => {
+			if (!this._started) return;
+			this._previewPixel();
+			requestAnimationFrame(frame);
+		};
+		frame();
+	}
 
-				video.setAttribute("width", width);
-				video.setAttribute("height", height);
-				canvas.setAttribute("width", width);
-				canvas.setAttribute("height", height);
-				streaming = true;
-			}
-		},
-		false
-	);
-
-	startbutton.addEventListener(
-		"click",
-		(ev) => {
-			takepicture();
-			ev.preventDefault();
-		},
-		false
-	);
-
-	clearphoto();
-}
-
-// Fill the photo with an indication that none has been
-// captured.
-
-function clearphoto() {
-	const context = canvas.getContext("2d");
-	context.fillStyle = "#AAA";
-	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	const data = canvas.toDataURL("image/png");
-	photo.setAttribute("src", data);
-}
-
-// Capture a photo by fetching the current contents of the video
-// and drawing it into a canvas, then converting that to a PNG
-// format data URL. By drawing it on an offscreen canvas and then
-// drawing that to the screen, we can change its size and/or apply
-// other changes before drawing it.
-
-function takepicture() {
-	const context = canvas.getContext("2d");
-	if (width && height) {
-		canvas.width = width;
-		canvas.height = height;
-		context.drawImage(video, 0, 0, width, height);
-
-		const data = canvas.toDataURL("image/png");
-		photo.setAttribute("src", data);
-
-		const sx = width / 2 | 0;
-		const sy = height / 2 | 0;
-		const sw = 1;
-		const sh = 1;
-		const pixel = context.getImageData(sx, sy, sw, sh);
-		// console.log(pixel);
-		document.getElementById('captured-color').style.backgroundColor = `rgb(${pixel.data[0]}, ${pixel.data[1]}, ${pixel.data[2]})`;
-	} else {
-		clearphoto();
+	_stop() {
+		this._started = false;
 	}
 }
-
-startup();
